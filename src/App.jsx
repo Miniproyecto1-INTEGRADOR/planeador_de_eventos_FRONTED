42c82fddc4b8dce7248d5c50453ded110776205a
Daniel Conda <daniel.conda@correounivalle.edu.co>
2026-09-21 23:16:39 -0500

    Actualiza rutas en App para redirigir a la página de "hoy" y añade nueva línea al final de CrearEvento

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import CrearEvento from './pages/CrearEvento.jsx'
 
 function App() {
-  return <BrowserRouter><Routes><Route path="/crear" element={<CrearEvento />} /><Route path="*" element={<Navigate to="/crear" replace />} /></Routes></BrowserRouter>
+  return <BrowserRouter><Routes><Route path="/hoy" element={<CrearEvento />} /><Route path="*" element={<Navigate to="/hoy" replace />} /></Routes></BrowserRouter>
 }
 
 export default App
